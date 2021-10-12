16c2ee8436bf198fe9b939189925404fb363f572
Brayden Ooi <brayden.ooi@hotmail.com>
2021-10-12 21:36:18 +0800

    feat(main): add swipes

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { SafeAreaView, StatusBar, useColorScheme } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import ItemListScreen from './src/screens/ItemListScreen';
 import { store, persistor } from './src/store';
@@ -11,18 +12,19 @@ const App = () => {
   const isDarkMode = colorScheme === 'dark';
 
   const backgroundStyle = {
-    // backgroundColor: isDarkMode ? "#eee" : "#ddd",
     flex: 1,
   };
 
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <GestureHandlerRootView style={backgroundStyle}>
           <SafeAreaView style={backgroundStyle}>
             <StatusBar
               barStyle={!isDarkMode ? 'light-content' : 'dark-content'} />
             <ItemListScreen />
           </SafeAreaView>
+        </GestureHandlerRootView>
       </PersistGate>
     </Provider>
   );
--- src/screens/ItemListScreen/ItemListScreen.tsx
+++ src/screens/ItemListScreen/ItemListScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   View,
@@ -8,12 +8,18 @@ import {
   useColorScheme,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { Swipeable } from 'react-native-gesture-handler';
 
 import Header from '../../presentation/Header';
 import Footer from '../../presentation/Footer';
 import SelectedItem from './SelectedItem';
 import AddItemForm from './AddItemForm';
 
+import {
+  selectItem,
+  deselectItem,
+  removeItem,
+} from '../../store/items/actions';
 import { loadColorScheme } from '../../store/system/actions';
 
 import stylesFn from './styles';
@@ -27,6 +33,7 @@ const ItemListScreen: React.FC<{}> = () => {
   const dispatch = useDispatch();
   const colorScheme = useColorScheme();
   const theme = useTheme();
+  const scrollViewRef = useRef();
 
   useEffect(() => {
     dispatch(loadColorScheme(colorScheme));
@@ -47,35 +54,67 @@ const ItemListScreen: React.FC<{}> = () => {
 
   const styles = stylesFn(theme, isHome);
 
+  const scrolltoBottom = () => scrollViewRef.current.scrollToEnd({ animated: true });
+
   return (
     <View style={styles.itemListScreen}>
       <Header />
       <View style={styles.contentContainer}>
-        <ScrollView style={styles.listContainer}>
+        <ScrollView style={styles.listContainer} ref={scrollViewRef}>
           {items
             .filter(item => isHome || item.isSelected)
             .map(item => (
-              <TouchableOpacity
+              <Swipeable
                 key={item.id}
-                style={[
-                  styles.listItemContainer,
-                  styles.addIconContainer,
-                  styles.listTitleContainer,
-                  { padding: 10 }
-                ]}
-                onPress={() => onItemPress(item.isSelected, item.id)}>
-                <Text ellipsizeMode="tail" numberOfLines={1}>
-                  {item.title}
-                </Text>
-                {isHome && (
-                  <Icon
-                    name="check"
-                    size={30}
-                    color="#fff"
-                    style={{ opacity: item.isSelected ? 1 : 0.5 }}
-                  />
-                )}
-              </TouchableOpacity>
+                renderRightActions={() => isHome
+                  ? (
+                    <View
+                      style={[
+                        styles.listTitleContainer,
+                        styles.addIconContainer,
+                        { justifyContent: 'space-evenly' },
+                      ]}>
+                      <Text style={styles.listTitleColor}>
+                        Continue to remove item
+                      </Text>
+                      <Icon
+                        name="trash-2"
+                        size={30}
+                        color={theme.dark_shadow}
+                      />
+                    </View>
+                  ) : (
+                    <></>
+                  )
+                }
+                onSwipeableRightOpen={() =>
+                  isHome ? dispatch(removeItem(item.id)) : {}
+                }>
+                <TouchableOpacity
+                  style={[
+                    styles.listItemContainer,
+                    styles.addIconContainer,
+                    styles.listTitleContainer,
+                    { padding: 10 },
+                  ]}
+                  activeOpacity={isHome ? 0.2 : 1}
+                  onPress={() => onItemPress(item.isSelected, item.id)}>
+                  <Text
+                    ellipsizeMode="tail"
+                    numberOfLines={1}
+                    style={[styles.listTitle, styles.listTitleColor]}>
+                    {item.title}
+                  </Text>
+                  {isHome && (
+                    <Icon
+                      name="check"
+                      size={30}
+                      color={theme.dark_shadow}
+                      style={{ opacity: item.isSelected ? 1 : 0.5 }}
+                    />
+                  )}
+                </TouchableOpacity>
+              </Swipeable>
             ))}
           {isHome && <AddItemForm />}
         </ScrollView>
